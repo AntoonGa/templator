bdf8410a48fe654a6d5a587cb0bf961bbf8cbe38
ORFIL\agarc <lskjfpoiu1_jhg@yopmail.com>
2024-09-21 21:11:04 +0200

    feat: settings

---
+++ src/templator/settings/__init__.py
@@ -0,0 +1,10 @@
+"""File: __init__.py | Author: username | date: Sat Sep 21 2024
+
+.. include:: readme.md
+Description: Setup for the project
+TODO @username:
+"""
+
+
+if __name__ == "__main__":
+    pass
