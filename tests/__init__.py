b33166a61e1261d1304af5bd4b8c8635dbffd24a
ORFIL\agarc <lskjfpoiu1_jhg@yopmail.com>
2024-09-22 12:09:06 +0200

    fix: precommit

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 """File: __init__.py | Author: username | date: Sat Sep 21 2024
 
 .. include:: readme.md
-Description: sets of unit tests.
+Description: sets of unit tests
 """
 
 if __name__ == "__main__":
