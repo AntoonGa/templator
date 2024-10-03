f2d8202d56a9ca32db4f9b902bbe38d5ed12579a
COMMUN\FAGN01241 <a.garcon.a@gmail.com>
2024-10-03 18:24:02 +0200

    fix: precommit config and justfile conflict

--- src/fastblob/api/blob_manager/endpoint.py
+++ src/fastblob/api/blob_manager/endpoint.py
@@ -37,7 +37,7 @@ def download(request: DownloadRequest) -> Response:
     Downloading a file from the Blob to your local system.
 
     Args:
-        request (DownloadRequest): Request parameters specifying the azeazeazeazezzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+        request (DownloadRequest): Request parameters specifying the azeazeazeazezzzzzzzzzzzzzzzzzzzzzzzzzzzzzazeazezzzzzzzzzzzzz
         file-name inside the Blob and the output-file name
         in your local system.
 
