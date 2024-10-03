9c646b77d3db51bbb5f4031ff2c9058f9407f185
COMMUN\FAGN01241 <a.garcon.a@gmail.com>
2024-10-03 18:38:48 +0200

    fix: precommit and ruff not working

--- src/fastblob/api/blob_manager/endpoint.py
+++ src/fastblob/api/blob_manager/endpoint.py
@@ -51,7 +51,7 @@ def download(request: DownloadRequest) -> Response:
 def list_file() -> dict:
     """Endpoint to handle the 'list' API
 
-    Shows a list of files present in the Blob storage                 sds dsdsds dsd sdssdfdfdfdfdfdsdsfgfgfgfgfdsdsfdfdfdfjssssssdfdfdfdfdssss
+    Shows a list of files present in the Blob storage
 
     Args:
         request (ListRequest): Empty Request
